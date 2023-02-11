493530f080d8db525a53f669dac59d9dfda7d8d9
Memex <robotastray@cantab.net>
2023-02-11 22:53:48 +0000

    different colour scheme

--- web_interface.py
+++ web_interface.py
@@ -17,7 +17,7 @@ with st.sidebar:
         "container": {"padding": "5!important", "background-color": "#fafafa"},
         "icon": {"color": "orange", "font-size": "25px"}, 
         "nav-link": {"font-size": "16px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
-        "nav-link-selected": {"background-color": "#02ab21"},
+        "nav-link-selected": {"background-color": "#41b3a3"},
     }
     )
 
